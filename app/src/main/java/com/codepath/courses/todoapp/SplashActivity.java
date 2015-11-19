436ec2322401e2125eaa1b53c518062febc0e9dc
deepaks <deepaks@ebay.com>
2015-11-18 21:57:43 -0800

    Added Splash Screen while starting the app

---
+++ app/src/main/java/com/codepath/courses/todoapp/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.codepath.courses.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private boolean _active = true;
+    // milliseconds
+    private int _splashTime = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch (Exception e) {
+
+                } finally {
+                    startActivity(new Intent(SplashActivity.this,
+                            MainActivity.class));
+                    finish();
+                }
+            }
+
+            ;
+        };
+        splashTread.start();
+    }
+}
